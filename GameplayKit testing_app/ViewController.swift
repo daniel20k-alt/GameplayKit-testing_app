efdd873b7cc5b62d96ecad301fdbd249a73bd009
Testing_author <testing_beta@mail.com>
2020-10-23 23:16:29 +0300

    Initial Commit

---
+++ GameplayKit testing_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameplayKit testing_app
+//
+//  Created by DDDD on 23/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
