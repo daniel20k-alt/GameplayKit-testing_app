68a78725d6f11798851d2e86776cda7eec1c4663
Testing_author <testing_beta@mail.com>
2020-10-27 23:38:17 +0200

    Implementing the methods chip(inColumn:row:) and set(chip:in:) to read the color and set the color.
    Determining whether the player can place a chip in a column with the nextEmptySlot(in:) method, by verifying whether it's empty.

--- GameplayKit testing_app/Board.swift
+++ GameplayKit testing_app/Board.swift
@@ -27,5 +27,36 @@ class Board: NSObject {
         }
         super.init()
     }
+    
+    
+    func chip(inColumn column: Int, row: Int) -> ChipColor {
+        return slots[row + column * Board.height]
+    }
 
+    
+    func set(chip: ChipColor, in column: Int, row: Int) {
+        slots[row + column * Board.height] = chip
+    }
+    
+    
+    func nextEmptySlot(in column: Int) -> Int? {
+        for row in 0 ..< Board.height {
+            if chip(inColumn: column, row: row) == .none { //check if there's at least one empty slot with .none
+                return row
+            }
+        }
+        return nil
+    }
+    
+    
+    func canMove(in column: Int) -> Bool {
+        return nextEmptySlot(in: column) != nil
+    }
+    
+    
+    func add(chip: ChipColor, in colum: Int) {
+        if let row = nextEmptySlot(in: colum) {
+            set(chip: chip, in: colum, row: row)
+        }
+    }
 }
