38a0a4fe5937b1475935de92885fc8b735b19e10
Testing_author <testing_beta@mail.com>
2020-10-31 23:29:44 +0200

    Detecting when a player wins by checking the squares on diagonals, vertically and horizontally.

--- GameplayKit testing_app/Board.swift
+++ GameplayKit testing_app/Board.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 enum ChipColor: Int {
     case none = 0
@@ -78,8 +79,42 @@ class Board: NSObject {
         return true
     }
     
-    func isWin(for player: Player) -> Bool {
+    func isWin(for player: GKGameModelPlayer) -> Bool {
+        let chip = (player as! Player).chip
+        
+        for row in 0 ..< Board.height {
+            for col in 0 ..< Board.width {
+                if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: 0) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 0, moveY: 1) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: 1) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: -1) {
+                    return true
+                }
+            }
+        }
+
         return false
+    }
+    
+    //checking the wins
+    func squaresMatch(initialChip: ChipColor, row: Int, col: Int, moveX: Int, moveY: Int) -> Bool {
+        
+        //leaving the method if at least 1 space is not ok
+        if row + (moveY * 3) < 0 { return false } //lower than height
+        if row + (moveY * 3) >= Board.height { return false } //higher than height
+        if col + (moveX * 3) < 0 { return false } //greather than width
+        if col + (moveX * 3) >= Board.width { return false }
+        
+        //if any of the above didnt check, continue the test
+        if chip(inColumn: col, row: row) != initialChip { return false }
+        if chip(inColumn: col + moveX, row: row + moveY) != initialChip { return false }
+        if chip(inColumn: col + (moveX * 2), row: row + (moveY * 2)) != initialChip { return false }
+        if chip(inColumn: col + (moveX * 3), row: row + (moveY * 3)) != initialChip { return false }
+        
+        return true
     }
     
 }
